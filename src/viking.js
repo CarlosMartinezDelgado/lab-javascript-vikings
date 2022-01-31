7fd8ff4d7f722dc2ccaee47e1b728cde6e9ee418
Carlos Martinez <Carlos.martdelgado@gmail.com>
2022-01-31 18:50:57 +0100

    Viking lab

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,62 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor (valueHealth, valueStrength) {
+    this.health =  valueHealth;
+    this.strength = valueStrength;
+  }
+attack = () => {
+  return this.strength;
+}
+receiveDamage = (theDamage) => {
+  this.health -= theDamage;
+}
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor (valueName, valueHealth, valueStrength) {
+    super (valueHealth, valueStrength);
+    this.name =  valueName;
+  }
+  receiveDamage = (theDamage) => {
+    this.health -= theDamage;
+    if (this.health <= 0) {
+      return `${this.name} has died in act of combat`;  
+    } else {
+      return `${this.name} has received ${theDamage} points of damage`;  
+    }
+  }
+  battleCry = () => {
+    return "Odin Owns You All!"
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  super (valueHealth, valueStrength) {
+  }
+
+  receiveDamage = (theDamage) => {
+    this.health -= theDamage;
+
+    if (this.health <= 0) {
+      return `A Saxon has died in combat`;
+    } else {
+      return `A Saxon has received ${theDamage} points of damage`
+    }
+}
+}
 
 // War
-class War {}
+class War {
+  constructor (vikingArmy, saxonArmy) {
+  this.addViking = "Viking";
+  this.addSaxon = "Saxon";
+  this.vikingAttack = "vAttack";
+  this.saxonAttack = "sAttack";
+  this.showStatus = "Status";
+  }
+}
 
 
 
